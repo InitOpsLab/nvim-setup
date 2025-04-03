94674b6b615f8c65b4d15e8e84fa21243b8c7cb2
Itay Russak <itay.russak@viz.ai>
2025-04-03 10:11:20 +0300

    Add lua configurations

--- configs/lua/lazy-plugins/plugins/tools.lua
+++ configs/lua/lazy-plugins/plugins/tools.lua
@@ -1,8 +1,44 @@
 return {
-  { "akinsho/toggleterm.nvim", version = "*" },
-  { "ThePrimeagen/harpoon" },
-  { "b0o/schemastore.nvim" },
-  { "numToStr/Comment.nvim", lazy = false },
-  { "windwp/nvim-autopairs", event = "InsertEnter" },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("config.toggleterm")
+    end,
+  },
+  {
+    "ThePrimeagen/harpoon",
+    config = function()
+      require("config.harpoon")
+    end,
+  },
+  {
+    "b0o/schemastore.nvim",
+    config = function()
+      require("config.schemastore")
+    end,
+  },
+  {
+    "numToStr/Comment.nvim",
+    lazy = false,
+    config = function()
+      require("Comment").setup()
+    end,
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup()
+    end,
+  },
+  {
+  "iamcco/markdown-preview.nvim",
+  build = "cd app && npm install",
+  ft = { "markdown" },
+  config = function()
+    require("config.markdown")
+  end,
+},
 }
 

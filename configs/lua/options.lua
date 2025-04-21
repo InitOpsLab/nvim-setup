c6729e95756c6a3ab44e72963b03c3355b67f39e
Itay Russak <itay.russak@viz.ai>
2025-04-21 12:33:22 +0300

    Update folding

--- configs/lua/options.lua
+++ configs/lua/options.lua
@@ -8,3 +8,14 @@ vim.opt.foldenable = true          -- important!
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 
+-- ~/.config/nvim/lua/options.lua
+
+vim.opt.clipboard = "unnamedplus"
+
+-- Treesitter-based folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = true
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+

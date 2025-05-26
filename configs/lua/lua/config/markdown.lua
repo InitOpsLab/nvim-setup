164b356d4acc1d455efdb70c931e238b0c5980f0
Itay Russak <itay.russak@viz.ai>
2025-05-26 15:54:20 +0300

    Add mmd files as markd down files

--- configs/lua/lua/config/markdown.lua
+++ configs/lua/lua/config/markdown.lua
@@ -7,10 +7,10 @@ vim.g.mkdp_command_for_global = 0
 vim.g.mkdp_open_to_the_world = 0
 vim.g.mkdp_browser = "default"
 vim.g.mkdp_echo_preview_url = 1
-vim.g.mkdp_port = "8080" 
+vim.g.mkdp_port = "8080"
 vim.g.mkdp_page_title = "Markdown Preview"
 
--- Optional Mermaid support
+-- Mermaid diagram support for Markdown Preview
 vim.g.mkdp_preview_options = {
   mermaid = {
     enable = true,

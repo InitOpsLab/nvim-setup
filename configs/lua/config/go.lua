723971d9a03cffaf824971c91f0b719570893286
Itay Russak <irussak3@gmail.com>
2025-07-02 15:28:32 +0300

    Update the goimports

--- configs/lua/config/go.lua
+++ configs/lua/config/go.lua
@@ -1,13 +1,12 @@
--- ~/.config/nvim/lua/config/go.lua
+-- lua/config/go.lua
 local ok, go = pcall(require, "go")
 if not ok then return end
 
 go.setup({
-  goimport        = "goimports",   -- or "gofumpt"
+  goimport        = "goimports",
   fillstruct      = "gopls",
   lsp_inlay_hints = { enable = true },
 })
 
--- Example keymap to run tests under cursor
 vim.keymap.set("n", "<leader>gt", ":GoTest<CR>", { noremap=true, silent=true })
 

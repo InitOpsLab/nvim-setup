98b6842df562efa19d5b2c3ca154aa10a3eef251
Itay Russak <irussak3@gmail.com>
2025-07-02 15:11:59 +0300

    Remove treesitter duplication

--- configs/lua/lazy-plugins/plugins/dev.lua
+++ configs/lua/lazy-plugins/plugins/dev.lua
@@ -4,13 +4,6 @@ return {
   { "hrsh7th/nvim-cmp" },
   { "hrsh7th/cmp-nvim-lsp" },
   { "L3MON4D3/LuaSnip" },
-  { "stevearc/conform.nvim" },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("config.treesitter")
-    end,
-  },
+  { "stevearc/conform.nvim"},
 }
 
